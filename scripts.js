56f81c5f1f56d7a520776a5d92906c666fa6c413
anurag-semwal <anuragsemwal.workmail@gmail.com>
2024-08-02 13:32:51 +0530

    Add calculate funtion to call basic math function based on operator

--- scripts.js
+++ scripts.js
@@ -10,3 +10,23 @@ function divide(a, b) {
 function multiply(a, b) {
   return a * b;
 }
+
+function calculate(num1, operator, num2) {
+  let output;
+  switch (operator) {
+    case "+":
+      output = add(num1, num2);
+      break;
+    case "-":
+      output = subtract(num1, num2);
+      break;
+    case "/":
+      output = divide(num1, num2);
+      break;
+    case "*":
+      output = multiply(num1, num2);
+      break;
+  }
+}
+
+calculate("2", "*", "5");
