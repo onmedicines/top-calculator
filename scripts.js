4c3650aa5f71af8f22857d8e263c8dba3cc002ee
anurag-semwal <anuragsemwal.workmail@gmail.com>
2024-08-02 21:39:32 +0530

    Reset calculator on pressing AC button

--- scripts.js
+++ scripts.js
@@ -4,12 +4,15 @@ const displayForEquation = document.querySelector(".equation");
 const numbers = document.querySelector(".numbers");
 const operators = document.querySelector(".operators");
 const equals = document.querySelector("#equals");
+const accumulatorClear = document.querySelector(".accumulator-clear");
+const clear = document.querySelector(".clear");
 
 // global variables
 let num1, num2;
 num1 = num2 = 0;
 let operator = "";
 let num1Entered = false;
+let operatorEntered = false;
 
 // calculation functions
 function add(a, b) {
@@ -72,9 +75,10 @@ function displayExpressionOnCalculator(str) {
 // event listeners
 numbers.addEventListener("click", (e) => {
   numberPressed = +e.target.id;
-  if (num1Entered === false) {
+  if (!num1Entered) {
     num1 = num1 * 10 + numberPressed;
   } else {
+    operatorEntered = false;
     num2 = num2 * 10 + numberPressed;
   }
   displayExpressionOnCalculator(numberPressed);
@@ -82,10 +86,19 @@ numbers.addEventListener("click", (e) => {
 
 operators.addEventListener("click", (e) => {
   num1Entered = true;
+  operatorEntered = true;
   operator = e.target.id;
   displayExpressionOnCalculator(operator);
 });
 
 equals.addEventListener("click", (e) => {
   displayAnswerOnCalculator();
+});
+
+accumulatorClear.addEventListener("click", () => {
+  num1 = num2 = 0;
+  num1Entered = false;
+  operator = "";
+  displayForAnswer.textContent = "";
+  displayForEquation.textContent = "";
 });
