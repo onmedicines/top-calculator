d6c5e3a1beea7b21defdee690b98df69af98da37
anurag-semwal <anuragsemwal.workmail@gmail.com>
2024-08-02 18:41:15 +0530

    Add code to perform basic calculation
    -add variables to take input for first number, second number and operator.
    -add functions to store the inputs in the respective local variables.
    -add function to call calculate() and return output on pressing equals button.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+// reference to elements
+const displayForAnswer = document.querySelector(".answer");
+const numbers = document.querySelector(".numbers");
+const operators = document.querySelector(".operators");
+const equals = document.querySelector("#equals");
+
+// global variables
+let num1, num2;
+let operator = "";
+let num1Entered = false;
+let equalsPressed = false;
+
+// calculation functions
 function add(a, b) {
   return a + b;
 }
@@ -14,20 +27,35 @@ function multiply(a, b) {
 function calculate(num1, operator, num2) {
   let output;
   switch (operator) {
-    case "+":
+    case "add":
       output = add(num1, num2);
       break;
-    case "-":
+    case "subtract":
       output = subtract(num1, num2);
       break;
-    case "/":
+    case "divide":
       output = divide(num1, num2);
       break;
-    case "*":
+    case "multiply":
       output = multiply(num1, num2);
       break;
   }
   return output;
 }
 
-calculate("2", "/", "5");
+// event listeners
+numbers.addEventListener("click", (e) => {
+  numberPressed = +e.target.id;
+  if (num1Entered === false) {
+    num1Entered = true;
+    num1 = numberPressed;
+  } else {
+    num2 = numberPressed;
+  }
+});
+
+operators.addEventListener("click", (e) => {
+  operator = e.target.id;
+});
+
+equals.addEventListener("click", (e) => {});
